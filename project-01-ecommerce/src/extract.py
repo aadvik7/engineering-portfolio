5efd49e7561dc8fe4e7405ece8f4e7a76c6a63be
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-04-20 16:12:59 +0530

    start loading csvs

--- project-01-ecommerce/src/extract.py
+++ project-01-ecommerce/src/extract.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import os
 
-# load raw csvs from data folder
-# TODO: load all 9 olist csvs
+DATA_PATH = "data/"
 
 def load_data():
-    pass
+    customers = pd.read_csv(DATA_PATH + "olist_customers_dataset.csv")
+    orders = pd.read_csv(DATA_PATH + "olist_orders_dataset.csv")
+    order_items = pd.read_csv(DATA_PATH + "olist_order_items_dataset.csv")
+
+    print(customers.shape)
+    print(orders.shape)
+
+    return customers, orders, order_items
