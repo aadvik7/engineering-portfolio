1d985c3b3b2e806678943f8928f004df689be6d0
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-04-22 19:41:31 +0530

    wip: add load script

--- project-01-ecommerce/src/load.py
+++ project-01-ecommerce/src/load.py
@@ -1,3 +1,21 @@
+import pandas as pd
 from sqlalchemy import create_engine
+from transform import transform
 
-# load cleaned data into postgres
+# TODO: move this to env later
+DB_URL = "postgres://postgres:password@localhost:5432/olist"
+
+def get_engine():
+    engine = create_engine(DB_URL)
+    return engine
+
+def load_to_db(df):
+    engine = get_engine()
+    print("loading to postgres...")
+    df.to_sql('orders_enriched', engine, if_exists='replace', index=False)
+    print("done")
+
+
+if __name__ == "__main__":
+    df = transform()
+    load_to_db(df)
