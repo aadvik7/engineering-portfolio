0c5ae742e3466567279d4a5b737cbc710b8c20e1
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-04-28 12:08:11 +0530

    add config and env example

---
+++ project-02-weather-etl/config.py
@@ -0,0 +1,6 @@
+CITIES = ['Delhi', 'Mumbai', 'Bangalore', 'Chennai', 'Kolkata']
+
+OWM_BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+# fetch interval in minutes
+FETCH_INTERVAL = 60
