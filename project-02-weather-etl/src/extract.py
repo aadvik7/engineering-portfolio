b46417c9e6be3efd3083688afa5d597ba637fe6a
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-04-30 16:00:59 +0530

    add city loop

--- project-02-weather-etl/src/extract.py
+++ project-02-weather-etl/src/extract.py
@@ -1,14 +1,17 @@
 import requests
+from config import CITIES
 
 API_KEY = "abc123replacethis"
 
 def fetch_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
-    print(response.status_code)
-    print(response.json())
-    return response.json()
+    data = response.json()
+    print(f"\n--- {city} ---")
+    print(data)
+    return data
 
 
 if __name__ == "__main__":
-    fetch_weather("Delhi")
+    for city in CITIES:
+        fetch_weather(city)
