874b1ef3f4d9c577ac111fb9249b68dc9b94ca31
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-04-30 21:00:14 +0530

    print raw json fields

--- project-02-weather-etl/src/extract.py
+++ project-02-weather-etl/src/extract.py
@@ -7,8 +7,15 @@ def fetch_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
+
+    # print specific fields to understand structure
     print(f"\n--- {city} ---")
-    print(data)
+    print("temp:", data['main']['temp'])
+    print("feels_like:", data['main']['feels_like'])
+    print("humidity:", data['main']['humidity'])
+    print("weather:", data['weather'][0]['description'])
+    print("wind speed:", data['wind']['speed'])
+
     return data
 
 
