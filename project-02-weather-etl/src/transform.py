8b9cb884aa36d227067e44630d73ad29ca2db49e
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-05-05 19:55:29 +0530

    start transform

--- project-02-weather-etl/src/transform.py
+++ project-02-weather-etl/src/transform.py
@@ -1,2 +1,13 @@
+from datetime import datetime
 
-# parse and clean raw api response
+def parse_weather(raw, city):
+    return {
+        'city': city,
+        'temp': raw['main']['temp'],
+        'feels_like': raw['main']['feels_like'],
+        'humidity': raw['main']['humidity'],
+        'pressure': raw['main']['pressure'],
+        'weather_desc': raw['weather'][0]['description'],
+        'wind_speed': raw['wind']['speed'],
+        'fetched_at': datetime.utcnow()
+    }
