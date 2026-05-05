8f697c60b5d389ee813070afc219bab1ece9c97f
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-05-05 19:35:10 +0530

    move api key to env, oops

--- project-02-weather-etl/src/extract.py
+++ project-02-weather-etl/src/extract.py
@@ -1,24 +1,18 @@
 import requests
+import os
+from dotenv import load_dotenv
 from config import CITIES
 
-API_KEY = "abc123replacethis"
+load_dotenv()
+API_KEY = os.getenv('OWM_API_KEY')
 
 def fetch_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
-    data = response.json()
-
-    # print specific fields to understand structure
-    print(f"\n--- {city} ---")
-    print("temp:", data['main']['temp'])
-    print("feels_like:", data['main']['feels_like'])
-    print("humidity:", data['main']['humidity'])
-    print("weather:", data['weather'][0]['description'])
-    print("wind speed:", data['wind']['speed'])
-
-    return data
+    return response.json()
 
 
 if __name__ == "__main__":
     for city in CITIES:
-        fetch_weather(city)
+        print(f"\n--- {city} ---")
+        print(fetch_weather(city))
