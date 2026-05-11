635890b5e5272eb98f1db90cc1bb2fdadca67e31
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-05-11 22:08:18 +0530

    start extract

--- project-03-airflow-news/src/extract.py
+++ project-03-airflow-news/src/extract.py
@@ -1,3 +1,22 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-# fetch news articles from newsapi
+load_dotenv()
+API_KEY = os.getenv('NEWS_API_KEY')
+
+def fetch_news(query="technology", language="en"):
+    url = "https://newsapi.org/v2/everything"
+    params = {
+        'q': query,
+        'language': language,
+        'apiKey': API_KEY
+    }
+    response = requests.get(url, params=params)
+    print(response.status_code)
+    print(response.json())
+    return response.json()
+
+
+if __name__ == "__main__":
+    fetch_news()
