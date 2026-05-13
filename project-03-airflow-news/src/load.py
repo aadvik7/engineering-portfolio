bef338d82324d60f2d096105289b567ad56a589f
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-05-13 23:11:17 +0530

    start load script

--- project-03-airflow-news/src/load.py
+++ project-03-airflow-news/src/load.py
@@ -1,3 +1,11 @@
+import os
+import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-# insert articles into postgres
+load_dotenv()
+
+DB_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
+def get_engine():
+    return create_engine(DB_URL)
